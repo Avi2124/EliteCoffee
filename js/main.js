262539e51359ff4c4f658c41438be89ba9e84dc9
Avi2124 <bharatservice1310@gmail.com>
2026-09-13 22:09:25 +0530

    Fix Responsive

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const navbarToggle = document.getElementById("navbarToggle");
+const navbarMenu = document.getElementById("navbarMenu");
+
+if (navbarToggle && navbarMenu) {
+    navbarToggle.addEventListener("click", () => {
+        navbarMenu.classList.toggle("active");
+
+        const isOpen = navbarMenu.classList.contains("active");
+        navbarToggle.setAttribute("aria-expanded", isOpen);
+
+        navbarToggle.innerHTML = isOpen
+            ? '<i class="fa fa-times"></i>'
+            : '<i class="fa fa-bars"></i>';
+    });
+}
