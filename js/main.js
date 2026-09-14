f1124df481b5316c8de96c171bcad55aece81439
Avi2124 <avikitaliya2018@gmail.com>
2026-09-14 12:11:11 +0530

    Fix main.js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@ const navbarToggle = document.getElementById("navbarToggle");
 const navbarMenu = document.getElementById("navbarMenu");
 
 if (navbarToggle && navbarMenu) {
+    navbarMenu.classList.remove("active");
+
     navbarToggle.addEventListener("click", () => {
         navbarMenu.classList.toggle("active");
 
         const isOpen = navbarMenu.classList.contains("active");
+
         navbarToggle.setAttribute("aria-expanded", isOpen);
 
         navbarToggle.innerHTML = isOpen
